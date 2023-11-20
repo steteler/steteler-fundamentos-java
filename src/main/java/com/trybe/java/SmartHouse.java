f91e9c040c53c81e2f2149c578574b592b9cbb6e
steteler <joaovictorkikuti@gmail.com>
2023-11-20 18:17:11 -0300

    feat: requisito 1 terminado

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -1,8 +1,44 @@
 package com.trybe.java;
 
 /**
- * Classe.
+ * Representa uma casa inteligente com controle da iluminação.
  **/
 public class SmartHouse {
 
+  private boolean isLightOn = false;
+
+  /**
+   * Liga a lâmpada, se ela estiver desligada.
+   *
+   * @throws IllegalStateException Se a lâmpada já estiver ligada.
+   */
+  public void ligarLampada() {
+    if (!isLightOn) {
+      isLightOn = true;
+    } else {
+      throw new IllegalStateException("A lampada já está ligada");
+    }
+  }
+
+  /**
+   * Desliga a lâmpada, se ela estiver ligada.
+   *
+   * @throws IllegalStateException Se a lâmpada já estiver desligada.
+   */
+  public void desligarLampada() {
+    if (isLightOn) {
+      isLightOn = false;
+    } else {
+      throw new IllegalStateException("A lampada já está desligada");
+    }
+  }
+
+  /**
+   * Verifica se a lâmpada está ligada.
+   *
+   * @return Retorna true se a lâmpada estiver ligada, senão ela retorna false.
+   */
+  public boolean isLampadaLigada() {
+    return isLightOn;
+  }
 }
